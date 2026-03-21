f86e64ac447ee8055de8071a9cdcb6ac562204ef
tylerwarre <tyler@warrens.one>
2026-03-20 19:40:58 -0600

    relay Initialization and shutdown code working

--- relay/relay.c
+++ relay/relay.c
@@ -8,6 +8,7 @@ int main() {
 
     result = wolfSSL_Init();
     if (result != WOLFSSL_SUCCESS) {
+        // Get error string from error number
         wolfSSL_ERR_error_string(result, errMsg);
         fprintf(stderr, "Failed to intialize wolfSSL: %s\n", errMsg);
         exit(result);
@@ -20,12 +21,25 @@ int main() {
     }
 
     // Load CA certificates
+    // Third parameter is NULL because we don't want to load a directory full of CA certs
     result = wolfSSL_CTX_load_verify_locations(ctx, "./certs/ca-cert.pem", NULL);
     if (result != WOLFSSL_SUCCESS) {
         wolfSSL_ERR_error_string(result, errMsg);
         fprintf(stderr, "Failed to load CA certs: %s\n", errMsg);
         exit(result);
     }
+
+    // Load relay private key
+    result = wolfSSL_CTX_use_PrivateKey_file(ctx, "./certs/relay-key.pem", SSL_FILETYPE_PEM);
+    if (result != WOLFSSL_SUCCESS) {
+        wolfSSL_ERR_error_string(result, errMsg);
+        fprintf(stderr, "Failed to load relay private key: %s\n", errMsg);
+        exit(result);
+    }
+
+    // Cleanup wolfSSL
+    wolfSSL_CTX_free(ctx);
+    wolfSSL_Cleanup();
 
     return 0;
 }
